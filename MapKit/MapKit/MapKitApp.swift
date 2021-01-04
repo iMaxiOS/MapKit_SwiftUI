82dd38a0f680846103acf81fb45eb0a516d84a50
Macostik <y.granchenko@gmail.com>
2021-01-04 11:27:55 +0200

    create app

---
+++ MapKit/MapKit/MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitApp.swift
+//  MapKit
+//
+//  Created by Maxim Granchenko on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
